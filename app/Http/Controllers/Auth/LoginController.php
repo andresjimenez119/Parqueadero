a30e874c80e8b0a3e00450aa58101cf38f5c5b6b
andresjimenez119 <andresjimenez119@gmail.com>
2020-04-13 19:59:13 -0500

    funcion cerrar sesion

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use App\Http\Requests;
-use Auth;
+use Illuminate\Http\Request;
+
 
 class LoginController extends Controller
 {
@@ -38,5 +38,12 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-
+/*
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->invalidate();
+        return $this->loggedOut($request) ?: redirect('/login');
+    }
+*/
 }
