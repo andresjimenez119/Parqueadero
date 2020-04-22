ae1e1eee69cc8bb506ac780eefd6a8488daecef6
andresjimenez119 <andresjimenez119@gmail.com>
2020-04-22 17:02:52 -0500

    tarifa

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -180,7 +180,7 @@ desired effect
                             <!-- Menu Toggle Button -->
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <!-- The user image in the navbar-->
-                                <img src="dist/img/img-160x160.jpg" class="user-image" alt="User Image">
+                                <img src="{{asset('dist/img/img-160x160.jpg')}}" class="user-image" alt="User Image">
                                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
                                 <span class="hidden-xs">{{ Auth::user()->name }}</span>
                             </a>
